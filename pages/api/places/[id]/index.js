1009d38a6cf78756802c7ccfeda99f19fa794fba
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-20 15:09:04 +0100

    read data

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,19 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "GET") {
+    try {
+      const place = await Place.findById(id);
+      response.status(200).json(place);
+      return;
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ status: "internal server error" });
+      return;
+    }
   }
-
-  response.status(200).json(place);
 }
